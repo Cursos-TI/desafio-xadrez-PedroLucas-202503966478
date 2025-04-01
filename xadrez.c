d87fb1152f6fe817953dd01d7f9c0edb8343d506
Pedro Lucas de Azevedo Dias <202503966478@alunos.newtonpaiva.edu.br>
2025-03-31 23:43:08 -0300

    nivel mestre concluido

--- xadrez.c
+++ xadrez.c
@@ -3,30 +3,57 @@
 // Move o bispo para a diagonal superior direita
 void moveBispo(int dist) {
     printf("Bispo:\n\n");
-    int i = 0;
-    do {
-        printf("Cima Direita\n");
-        i++;
-    } while (i < dist);
+    for (int i = 0; i < dist; i++) {
+        for (int j = 0; j < 1; j++) {
+            printf("Cima Direita\n");
+        }
+    }
     printf("\n");
+}
+
+// funcao auxiliar para moveTorre
+void _moveTorre(int dist) {
+    if (dist < 1) {
+        return;
+    }
+
+    printf("Direita\n");
+    _moveTorre(dist-1);
 }
 
 // Move a torre para a direita
 void moveTorre(int dist) {
     printf("Torre:\n\n");
-    int i = 0;
-    while (i < dist) {
-        printf("Direita\n");
-        i++;
-    }
+    _moveTorre(dist);
     printf("\n");
+}
+
+// funcao auxiliar para moveDama
+void _moveDama(int dist) {
+    if (dist < 1) {
+        return;
+    }
+
+    printf("Esquerda\n");
+    _moveDama(dist-1);
 }
 
 // Move a Dama para a esquerda
 void moveDama(int dist) {
     printf("Dama:\n\n");
+    _moveDama(dist);
+    printf("\n");
+}
+
+void moveCavalo(int dist) {
+    printf("Cavalo:\n\n");
     for (int i = 0; i < dist; i++) {
-        printf("Esquerda\n");
+        printf("Direita\n");
+        int j = 0;
+        while (j < 2) {
+            printf("Cima\n");
+            j++;
+        }
     }
     printf("\n");
 }
@@ -35,10 +62,12 @@ int main() {
     const int dist_bispo = 5;
     const int dist_torre = 5;
     const int dist_dama = 8;
+    const int dist_cavalo = 1;
 
     moveBispo(dist_bispo);
     moveTorre(dist_torre);
     moveDama(dist_dama);
+    moveCavalo(dist_cavalo);
 
     // Agarda o ENTER para finalizar a exexução
     char buff;
